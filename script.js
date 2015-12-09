ced877e3831661b9c768d2af201ac15c43f4df14
Matej <matej.bellus@gmail.com>
2015-12-09 18:26:22 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        NDR ad block
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Block ad on http://www.navratdoreality.com/index.php
+// @author       Horkyze
+// @match        http://www.navratdoreality.com/index.php
+// @grant        none
+// ==/UserScript==
+/* jshint -W097 */
+'use strict';
+
+var tds = document.getElementsByTagName("td");
+var max = tds.length;
+tds[0].style.background = "black";
+tds[max - 1].style.background = "black";
